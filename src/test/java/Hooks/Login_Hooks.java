c8cc702cdac06cd36af0d86eb8ff3c94cad88e16
Logeshwaran <testmail.loge@gmail.com>
2026-09-08 13:23:56 +0530

    Changing screenshot method and inlcuding assertion error to verify failure screenshot

--- src/test/java/Hooks/Login_Hooks.java
+++ src/test/java/Hooks/Login_Hooks.java
@@ -1,6 +1,5 @@
 package Hooks;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -18,34 +17,38 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class Login_Hooks extends BaseClass {
-	
-	
-	@Before ("@login")
+
+	@Before("@login")
 	public void start_browser() throws InterruptedException {
 		EdgeOptions option = new EdgeOptions();
 		option.addArguments("--start-maximized");
-		
-		
+
 		setDriver(new EdgeDriver(option));
 		getDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	}
-	
-	
+
 	@After
-	public void close_session(Scenario scenario ) throws IOException {
-		if(scenario.isFailed())
-		{
+	public void close_session(Scenario scenario) throws IOException {
+		if (scenario.isFailed()) {
 			TakesScreenshot ts = (TakesScreenshot) getDriver();
+
 			File source = ts.getScreenshotAs(OutputType.FILE);
+
 			String scenario_name = scenario.getName().replace(" ", "_");
+
 			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
 			String file_name = scenario_name + "_" + timeStamp + ".png";
-			File file_destination = new File("target/Screenshots/"+file_name);
-			if (!file_destination.exists()) {
-				file_destination.mkdirs();
+
+			File screenshot_folder = new File("target/Screenshots");
+
+			if (!screenshot_folder.exists()) {
+				screenshot_folder.mkdirs();
 			}
-			FileHandler.copy(source,file_destination);
-			
+
+			File file_destination = new File(screenshot_folder, file_name);
+
+			FileHandler.copy(source, file_destination);
 		}
 		getDriver().quit();
 		unload();
