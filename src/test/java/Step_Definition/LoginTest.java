c53ad8c55d1176dcb1b7c1f6d28e8ba8ff40aaea
Logeshwaran <testmail.loge@gmail.com>
2026-09-09 00:02:16 +0530

    // Jenkins webhook practice comment added

--- src/test/java/Step_Definition/LoginTest.java
+++ src/test/java/Step_Definition/LoginTest.java
@@ -32,7 +32,7 @@ public class LoginTest extends BaseClass {
 		dashboard_obj.verify_login();
 
 		System.out.println("Login verification completed successfully");
-		
+		// Jenkins webhook practice
 
 	}
 
